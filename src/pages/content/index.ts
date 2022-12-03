7eb792143b463311f0f5b05b5ddeec4dcdee52cb
G4den <gade1201@gmail.com>
2022-12-03 12:49:34 -0500

    Example of import not working when used in both content and popup

--- src/pages/content/index.ts
+++ src/pages/content/index.ts
@@ -1,4 +1,9 @@
+import { ethers } from 'ethers';
+
 try {
+  const wei = "1000000000000000000";
+  const ether = ethers.utils.formatEther(wei);
+  console.log(`${wei} wei = ${ether} ether`);
   console.log('content script loaded');
 } catch (e) {
   console.error(e);
